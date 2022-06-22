23328bb0c7487b47c2ef4025e39bbecef1b7860e
jimxjim <jim74255@gmail.com>
2022-06-22 22:42:04 +0800

    i18n and pages init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import 'normalize.css';
 import '../styles/globals.css'
+import { appWithTranslation } from 'next-i18next';
+import nextI18NextConfig from '../next-i18next.config.js';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default appWithTranslation(MyApp, nextI18NextConfig)
---
+++ pages/delegator/index.jsx
@@ -0,0 +1,43 @@
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+export default function Home() {
+  const { t } = useTranslation()
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Delegator</title>
+        <meta name="description" content="Tutorial for being a delegator in the Graph ecosystem" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          {t('delegator')}
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
+});
